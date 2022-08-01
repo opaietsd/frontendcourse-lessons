dbd18c09dd175f292832c61cd6e48b2f3a546f63
Dariia <opaetsd@gmail.com>
2022-08-01 22:28:24 +0300

    Begginning of the Calculator project

---
+++ Lesson 28/assets/js/main.js
@@ -0,0 +1,54 @@
+const calculatorScreen = document.getElementById("input");
+const calculatorButton = document.getElementsByClassName("number");
+
+//calculatorScreen.innerText = "The text that should be displayed";
+
+function one(){
+    calculatorScreen.innerText = "1";
+}
+
+function two(){
+    calculatorScreen.innerText = "2";
+}
+
+function three(){
+    calculatorScreen.innerText = "3";
+}
+
+function four(){
+    calculatorScreen.innerText = "4";
+}
+
+function five(){
+    calculatorScreen.innerText = "5";
+}
+
+function six(){
+    calculatorScreen.innerText = "6";
+}
+
+function seven(){
+    calculatorScreen.innerText = "7";
+}
+
+function eight(){
+    calculatorScreen.innerText = "8";
+}
+
+function nine(){
+    calculatorScreen.innerText = "9"
+}
+
+function zero(){
+    calculatorScreen.innerText = "0";
+}
+
+function delet(){
+    calculatorScreen.innerText = " ";
+}
+//calculatorButton.addEventlistener("click", () =>{
+//    calculatorScreen.innerText = "Daria"
+//} );
+
+
+
