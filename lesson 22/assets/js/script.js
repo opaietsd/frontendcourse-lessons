4b9e478fbb9840260482f2a8bb0a04034ecd8370
Dariia <opaetsd@gmail.com>
2022-08-11 21:08:30 +0300

    lesson 22 homework

--- lesson 22/assets/js/script.js
+++ lesson 22/assets/js/script.js
@@ -1,3 +1,19 @@
+//_______________Homework_______________
+
+//_______________Task 1_________________
+
+/*var num1 = prompt("Write num1");
+var num2 = prompt("Write num2");
+var num3 = prompt("Write num3");
+function check(){
+    if ( "num1" === "num2" && "num2" === "num3"){
+        return "True"
+    } else {
+        return "False"
+    }
+}
+
+alert(check())*/
 
 
 
