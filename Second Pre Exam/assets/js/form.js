f37d85afafa04808bda728dce87be364411ff4c9
Dariia <opaetsd@gmail.com>
2022-08-15 19:47:55 +0300

    i have started the mobile version

---
+++ Second Pre Exam/assets/js/form.js
@@ -0,0 +1,11 @@
+//const name = document.getElementById("name").value
+//const number = document.getElementById("tel");
+//const question = document.getElementById("quetion");
+//const button = document.getElementById("save");
+
+//function newUser(){
+//     name = inputField.getAttribute("name")
+//
+//     console.log(name)
+//}
+
