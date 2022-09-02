2ca4f03575b83af1d90f36c44f141d9590740cf0
Dariia <opaetsd@gmail.com>
2022-09-02 22:28:07 +0300

    Started the Todo from th scretch +ToDo form

---
+++ lesson-33/react-todo-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>Todo App</h1>
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
