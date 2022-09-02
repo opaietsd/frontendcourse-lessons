7fe22778032d3ac2ac097414f10145f62e670182
Dariia <opaetsd@gmail.com>
2022-09-02 21:11:05 +0300

    JS homework

--- lesson 22/assets/js/script.js
+++ lesson 22/assets/js/script.js
@@ -15,6 +15,82 @@ function check(){
 
 alert(check())*/
 
+//_______________Task 2_________________
+
+//let number = prompt("Write a five-unit number");
+//
+//function checkNumber(number){
+//     var len = number.length;
+//
+//     for(var i = 0; i < len/2; i++){
+//        if(number[i] !== number[len - 1 - i]){
+//            return false;
+//        }
+//     }
+//    return true;
+//}
+//alert (checkNumber(number));
+
+//_______________Task 3__________________
+
+// let change = prompt("How much money(USD) you want to change");
+// let val = prompt("Chose the currency(Only capital letters!!!)");
+
+// function exhange(change, val){
+//     let eur = 1;
+//     let sek = 10.78;
+//     let aud = 1.47;
+//     let cad = 1.31;
+
+//    if(val = "EUR"){
+//      result = change * eur;
+//    } else if (val = "SEK"){
+//     result = change * sek;
+//    } else if(val = "AUD"){
+//     result = change * aud;
+//    } else if(val = "CAD"){
+//     result = change * cad;
+//    } else{
+//     result = "Are tou Dump? Give me the currency."
+//    }
+//    return result;
+// }
+
+// alert ( exhange(change, val));
+
+//_______________Task 4__________________
+
+// let lengthCirk = prompt("What is the length of a circumference?");
+// let perimperSqure = prompt("What is the perimeter of a square?");
+
+// function checkFit(a, b){
+//     if( a < b ){
+//        result = "It will fit";
+//     } else {
+//         result = "It will not fit";
+//     }
+//     return result;
+// }
+// // Sorry I just couldn't find the right formula
+// alert (checkFit(lengthCirk, perimperSqure));
+
+
+//_______________Task 5__________________
+// let date = prompt("Write down the date");
+// let month = prompt("thite dowm the month");
+// let year = prompt("Write down the year");
+
+// function addition(){
+//     newDate = date.getDate() + 1;
+//     newMonth = month.getMonth() + 1;
+//     newYear = year.getFullYear() + 1;
+    
+//     return newDate, newMonth, newYear;
+// }
+
+ 
+// alert ( addition(date, month, year));
+
 
 
 
