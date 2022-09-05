b6c24804b7779d39e5054aa30ea1444ef2583145
Dariia <opaetsd@gmail.com>
2022-09-05 17:24:05 +0300

    added: -todo; -removeIcon; -editIcon; -linked todo in todoList

--- lesson-33/react-todo-app/src/components/TodoList.js
+++ lesson-33/react-todo-app/src/components/TodoList.js
@@ -1,22 +1,37 @@
-import React, {useState} from 'react'
-import TodoForm from './TodoForm'
+import React, {useState} from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
 
     const addTodo = todo => {
-        if(!todo.text || /^\s*$.test(todo.text)) {
-            return
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+           return 
         }
 
-        const newTodos = [todo, ...todos]
+        const newTodos = [todo, ...todos];
 
-        setTodos(newTodos)
+        setTodos(newTodos);
     }
+
+    const completeTodo = id => {
+      let updatedTodos = todos.map(todo =>{
+        if (todo.id === id) {
+          todo.isComplete = !todo.isComplete
+        }
+        return todo
+      })
+      setTodos(updatedTodos);
+    }
+
   return (
     <div>
       <h1>What's the plan for Today?</h1>
       <TodoForm onSubmit={addTodo} />
+      <Todo 
+      todos={todos}
+      completeTodo={completeTodo} />
     </div>
   )
 }
