4c4bfa66553a2af1b21801aa8bac1d040b1e5d7f
Dariia <opaetsd@gmail.com>
2022-09-05 16:38:38 +0300

    Satrted lesson's 26 homework

---
+++ lesson-23/assets/script.js
@@ -0,0 +1,12 @@
+//________________lms Lesson 26 Homework_______________
+
+//___________________Task 1___________________________
+
+// let number = [prompt("Write a number")]
+
+// for (let i = 1; i <= number; i++){
+//     console.log (i + parseInt(number))
+// }
+
+//___________________Task 2___________________________
+
